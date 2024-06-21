c66e824b3d8d6b182690c8458f63a80e97ce3332
thatanoslord <sabbarmoussa@gmail.Com>
2024-06-20 19:05:56 -0700

    changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ *  main - this is the main function
+ *  Return:  always O
+ *  /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
