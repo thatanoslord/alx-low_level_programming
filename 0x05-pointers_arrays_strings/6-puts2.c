92a56ef754f472defe2b16003518dd2b6634b367
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-07-02 03:09:09 +0100

    put6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ *  * puts2 - prints every other character
+ *   * @str: string pointer
+ *    */
+
+void puts2(char *str)
+{
+		int i;
+
+		int n = strlen(str);
+
+		for (i = 0; i < n; i++)
+		{
+		if (i % 2 == 0)
+		{	
+		_putchar(str[i]);
+		}
+		}
+		_putchar('\n');
+}
