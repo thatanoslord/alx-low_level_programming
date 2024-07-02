936c00bcafd91effe741eb597b4bb7ea6be2c3af
thatanoslord <sabbarmoussa@gmail.Com>
2024-07-01 19:51:50 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints an array
+ * @a: array
+ * @n: array size
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
