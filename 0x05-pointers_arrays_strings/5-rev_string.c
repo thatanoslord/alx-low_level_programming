9bdfeb42169d72e3e09cd4e03063550646a56dc7
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-07-02 03:11:10 +0100

    problem betty resolve

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
-* rev_string - reverses a string
-* @s: string pointer
-*/
+ *  * rev_string - reverses a string
+ *   * @s: string pointer
+ *    */
 
 void rev_string(char *s)
 {
-	int i, n = _strlen(s);
+		int i, n = _strlen(s);
 
-	char temp;
+			char temp;
 
-	for (i = 0; i < n / 2; i++)
-	{
-	temp = s[i];
-	s[i] = s[n - i - 1];			
-	s[n - i - 1] = temp;			
-	}
+				for (i = 0; i < n / 2; i++)
+						{
+									temp = s[i];
+											s[i] = s[n - i - 1];
+													s[n - i - 1] = temp;
+														}
 }
