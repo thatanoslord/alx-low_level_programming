6c1d699cc57f698212785d0d77241a523268a1f1
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-07-02 03:30:29 +0100

    betty style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,16 @@
 
 void puts2(char *str)
 {
-		int i;
+	int i;
 
-		int n = strlen(str);
+	int n = strlen(str);
 
-		for (i = 0; i < n; i++)
-		{
+	for (i = 0; i < n; i++)
+	{
 		if (i % 2 == 0)
-		{	
-		_putchar(str[i]);
+		{
+			_putchar(str[i]);
 		}
-		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
