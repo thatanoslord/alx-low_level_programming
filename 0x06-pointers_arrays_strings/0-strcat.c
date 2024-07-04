dc97ee10e38617530e367f83ba6401c3881e12a7
thatanoslord <sabbarmoussa@gmail.Com>
2024-07-04 08:22:28 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates strings
+ * @dest: destination
+ * @src: input
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int destLen = strlen(dest), i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[destLen++] = src[i];
+	}
+	dest[destLen] = '\0';
+	return (dest);
+}
