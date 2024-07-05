8b7c2f9fdcd4657cf3706ab38811bf30ff84d20d
thatanoslord <sabbarmoussa@gmail.Com>
2024-07-04 19:21:57 -0700

    before last

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/* Function prototype for print_number */
+void print_number(int n);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
+
