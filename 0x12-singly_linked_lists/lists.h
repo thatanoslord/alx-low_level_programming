d6d3b2708f9d74206432009881eb8af2924469f2
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-08-02 00:55:29 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,15 +8,15 @@
 /**
  * struct list_t - singly linked list type
  * @str: node value
- * @length: node value length
+ * @len: node value length
  * @next: next node
  */
 
 typedef struct list_t
 {
-    char *str;
-    unsigned int length;
-    struct list_t *next;
+	char *str;
+	unsigned int len;
+	struct list_t *next;
 } list_t;
 
 size_t print_list(const list_t *h);
