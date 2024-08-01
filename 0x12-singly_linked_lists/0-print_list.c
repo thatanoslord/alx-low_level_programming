00125610a00eb118d9b7ece7d3f446fc9d0a9aad
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-08-02 00:57:36 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t length = 0;
+	size_t length = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[%d] %s\n", 0, "(nil)");
-        else
-            printf("[%d] %s\n", h->length, h->str);
+	while (h)
+	{
+		if (!h->str)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->length, h->str);
 
-        length++;
-        h = h->next;
-    }
+		length++;
+		h = h->next;
+	}
 
-    return (length);
+	return (length);
 }
