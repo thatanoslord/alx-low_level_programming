e0aa750b4da75701f0dd31e70549beff59a1b8ac
thatanoslord <sabbarmoussa@gmail.Com>
2024-08-01 12:31:03 -0700

    added thedir

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints a list
+ * @h: list pointer
+ * Return: list length
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t length = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->length, h->str);
+
+		length++;
+		h = h->next;
+	}
+
+	return (length);
+}
+
