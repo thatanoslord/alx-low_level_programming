3d298150946d5210c0d1c3065b6820af9f50aaf2
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-08-02 01:00:37 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,21 +8,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *n_node;
-    size_t length;
+	list_t *n_node;
+	size_t length;
 
-    n_node = malloc(sizeof(list_t));
+	n_node = malloc(sizeof(list_t));
 
-    if (!n_node)
-        return (NULL);
+	if (!n_node)
+		return (NULL);
 
-    for (length = 0; str[length]; length++)
-        ;
+	for (length = 0; str[length]; length++)
+		;
 
-    n_node->str = strdup(str);
-    n_node->length = length;
-    n_node->next = *head;
-    *head = n_node;
+	n_node->str = strdup(str);
+	n_node->len = length;
+	n_node->next = *head;
+	*head = n_node;
 
-    return (*head);
+	return (*head);
 }
