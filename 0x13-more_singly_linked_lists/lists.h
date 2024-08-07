512a0c5734fb5cbb3fd172a033e069afd9d1ae49
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-07 02:05:19 +0100

    new adds to lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int sum_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int pop_listint(listint_t **head);
 void free_listint2(listint_t **head);
 void free_listint(listint_t *head);
