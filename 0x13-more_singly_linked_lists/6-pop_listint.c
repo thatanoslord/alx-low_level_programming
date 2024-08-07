4b23283b760996224dc495592e38cd7f748ac374
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-07 02:03:14 +0100

    new modifs

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 /**
- *  * pop_listint - deletes first element of a list
- *   * @head: start node
- *    * Return: value of deleted node
- *     */
+* pop_listint - deletes first element of a list
+* @head: start node
+* Return: value of deleted node
+*/
 
 int pop_listint(listint_t **head)
 {
-		listint_t *deleted_node = *head;
-			int n;
+	listint_t *deleted_node = *head;
+	int n;
 
-				if (!deleted_node)
-							return (0);
+	if (!deleted_node)
+	return (0);
 
-					*head = (*head)->next;
-						n = deleted_node->n;
+	*head = (*head)->next;
+	n = deleted_node->n;
 
-							free(deleted_node);
-								return (n);
+	free(deleted_node);
+	return (n);
 }
