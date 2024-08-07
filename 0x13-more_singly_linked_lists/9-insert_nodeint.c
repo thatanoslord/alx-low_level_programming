8eb2d0f9136d6e9a36d21d5cb78c31e244611053
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-07 02:21:43 +0100

    10th tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,40 @@
 #include "lists.h"
 /**
- *  * insert_nodeint_at_index - inserts node and given index
- *   * @head: start node
- *    * @idx: index
- *     * @n: data of the node
- *      * Return: updated list
- *       */
+* insert_nodeint_at_index - inserts node and given index
+* @head: start node
+* @idx: index
+* @n: data of the node
+* Return: updated list
+*/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-		listint_t *current = *head, *new_node;
-			unsigned int itr;
+	listint_t *current = *head, *new_node;
+	unsigned int itr;
 
-				new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-					if (!new_node)
-								return (NULL);
+	if (!new_node)
+	return (NULL);
 
-						new_node->n = n;
+	new_node->n = n;
 
-							if (idx == 0)
-									{
-												new_node->next = current;
-														*head = new_node;
-																return (new_node);
-																	}
+	if (idx == 0)
+	{
+	new_node->next = current;
+	*head = new_node;
+	return (new_node);
+	}
+	for (itr = 0; itr < idx - 1; itr++)
+	{
+	if (current == NULL || current->next == NULL)
+	return (NULL);
 
-								for (itr = 0; itr < idx - 1; itr++)
-										{
-													if (current == NULL || current->next == NULL)
-																	return (NULL);
+	current = current->next;
+	}
 
-															current = current->next;
-																}
+	new_node->next = current->next;
+	current->next = new_node;
 
-									new_node->next = current->next;
-										current->next = new_node;
-
-											return (new_node);
+	return (new_node);
 }
