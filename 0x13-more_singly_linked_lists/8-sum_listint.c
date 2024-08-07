5e2baafcd06f51cc3d68a4c4bd34cc3e08d232d3
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-08-07 02:15:26 +0100

    8th task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 /**
- *  * sum_listint - returns sum of all data in the linked list
- *   * @head: head node
- *    * Return: cumulative sum
- *     */
+* sum_listint - returns sum of all data in the linked list
+* @head: head node
+* 
+* Return: cumulative sum
+*/
 
 int sum_listint(listint_t *head)
 {
-		int sum = 0;
+	int sum = 0;
 
-			while (head)
-					{
-								sum += head->n;
-										head = head->next;
-											}
-				return (sum);
+	while (head)
+	{
+	sum += head->n;
+	head = head->next;
+	}
+	return (sum);
 }
