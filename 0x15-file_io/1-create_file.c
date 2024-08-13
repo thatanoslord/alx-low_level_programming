5672d0692eb43c38a97dbae82a2f14ab25e6bb75
thatanoslord <sabbarmoussa@gmail.Com>
2024-08-13 09:11:02 -0700

    resolver sec prob

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,28 @@
 #include "main.h"
 #include <fcntl.h>
-<<<<<<< HEAD
-
-=======
->>>>>>> 6188a73ab924c832a1ba54c4f727cd6b442378dd
 /**
-* create_file - Creates a file with the specified name and content.
-* @filename: A pointer to the name of the file to be created.
-* @text_content: A pointer to the string to write to the file.
-*
-* Return: 1 on success, -1 on failure.
-<<<<<<< HEAD
-*         -1 if filename is NULL or if the file cannot be created.
-*         1 if the file is created and written successfully.
-=======
-*	-1 if filename is NULL or if the file cannot be created.
-*	1 if the file is created and written successfully.
->>>>>>> 6188a73ab924c832a1ba54c4f727cd6b442378dd
-*/
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: 1 if success, -1 if fail
+ */
 
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t openBuff, writeBuff, len = 0;
 
 	if (!filename)
-	return (-1);
+		return (-1);
 
 	if (text_content)
-	for (; text_content[len]; len++)
-		;
+		for (; text_content[len]; len++)
+			;
 
 	openBuff = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writeBuff = write(openBuff, text_content, len);
 
 	if (openBuff == -1 || writeBuff == -1)
-	return (-1);
+		return (-1);
 
 	close(openBuff);
 	return (1);
