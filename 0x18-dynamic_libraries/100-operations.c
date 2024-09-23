e2f428f99dc67e0fcc9a29f6061f595ef080a9e5
Moussa Sabbar <sabbarmoussa@gmail.com>
2024-09-24 00:59:16 +0100

    new fthing

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ *  * add - add 2 integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     * Return: result
+ *      */
+
+int add(int a, int b)
+{
+		return (a + b);
+}
+
+/**
+ *  * sub - substitute 2 integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     * Return: result
+ *      */
+
+int sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
+ *  * mul - multiply 2 integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     * Return: result
+ *      */
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
+ *  * div - divide 2 integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     * Return: result
+ *      */
+
+int div(int a, int b)
+{
+		return (a / b);
+}
+
+/**
+ *  * mod - modulus 2 integers
+ *   * @a: integer 1
+ *    * @b: integer 2
+ *     * Return: result
+ *      */
+
+int mod(int a, int b)
+{
+		return (a % b);
+}
