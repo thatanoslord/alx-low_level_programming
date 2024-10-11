9fca35e51ad4bd8155f3d023a1495f0ad8d71d26
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-10-11 23:36:39 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,11 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
 
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -33,13 +35,14 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_node_t* create_new_node(const char* key, const char* value);
+int hash_table_set(hash_table_t* ht, const char* key, const char* value);
+char* hash_table_get(const hash_table_t* ht, const char* key);
+void hash_table_print(const hash_table_t* ht);
+void hash_table_delete(hash_table_t* ht);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
+void free_hash_list(hash_node_t *head);
 
 #endif
