edc31790a350fb1f8bc7b0a65ba8fc25f63bafc8
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2025-01-28 23:50:07 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,60 +1,29 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints the contents of an array.
- * @array: The source of the array to print.
- * @l: The left index of the array.
- * @r: The right index of the array.
- */
-void print_array(int *array, size_t l, size_t r)
-{
-	size_t i;
-
-	if (array)
-	{
-		printf("Searching in array: ");
-		for (i = l; i < l + (r - l + 1); i++)
-			printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
-	}
-}
-
-/**
- * binary_search_index - Searches a value in a sorted array using \
- * a binary search.
- * @array: The array to search in.
- * @l: The left index of the array.
- * @r: The right index of the array.
- * @value: The value to look for.
- *
- * Return: The first index of the value in the array, otherwise -1.
- */
-int binary_search_index(int *array, size_t l, size_t r, int value)
-{
-	size_t m;
-
-	if (!array)
-		return (-1);
-	print_array(array, l, r);
-	m = l + ((r - l) / 2);
-	if (l == r)
-		return (*(array + m) == value ? (int)m : -1);
-	if (value < *(array + m))
-		return (binary_search_index(array, l, m - 1, value));
-	else if (value == *(array + m))
-		return ((int)m);
-	else
-		return (binary_search_index(array, m + 1, r, value));
-}
-
-/**
- * binary_search - Searches a value in a sorted array using a binary search.
+ * linear_search - Searches a value in an array using a linear search.
  * @array: The array to search in.
  * @size: The length of the array.
  * @value: The value to look for.
  *
- * Return: The index of the value in the array, otherwise -1.
+ * Return: The first index of the value in the array, otherwise -1.
  */
-int binary_search(int *array, size_t size, int value)
+
+int linear_search(int *array, size_t size, int value)
 {
-	return (binary_search_index(array, 0, size - 1, value));
+	size_t i;
+
+	for (i = 0; (i < size) && (array); i++)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		}
+	}
+	return (-1);
 }
